d9cdfb90e4418926f30ec15c4aad36886999ed82
Meng Zhang <zhang4@fzi.de>
2021-09-30 00:16:17 +0200

    modified path

--- main.py
+++ main.py
@@ -130,8 +130,8 @@ def fit_style_transfer(style_image, content_image, style_weight=1e-2, content_we
 
 
 if '__name__' == '__main__':
-    content_path = '/home/mzhang/Neural Style Transfer/content.png'
-    style_path = '/home/mzhang/Neural Style Transfer/content.png'
+    content_path = '/home/mzhang/Style_Transfer/content.png'
+    style_path = '/home/mzhang/Style_Transfer/content.png'
 
     # display the content and style image
     content_image, style_image = utils.load_images(content_path, style_path)
