60fc9fe22f4a3139dd4b199ddb0187854048f083
Meng Zhang <zhang4@fzi.de>
2021-09-30 12:05:43 +0200

    try VGG19

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ content_layers = ['block5_conv2']
 
 style_layers = ['block1_conv1',
                 'block2_conv1',
-                'block3_conv1'
-                'block4_conv1'
+                'block3_conv1',
+                'block4_conv1',
                 'block5_conv1']
 
 total_layers = content_layers + style_layers
