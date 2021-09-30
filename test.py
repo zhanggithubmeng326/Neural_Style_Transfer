05a4e243a4ac827349533d2f21fc7d7f2331b58f
Meng Zhang <zhang4@fzi.de>
2021-09-30 17:57:42 +0200

    added test.py

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 import utils
 
-if __name__ == '__main__'
+
 content_path = '/home/mzhang/Style_Transfer/content.jpg'
 style_path = '/home/mzhang/Style_Transfer/style.jpg'
 
 c_img = utils.load_image(content_path)
 s_img = utils.load_image(style_path)
 
-utils.show_images_with_objects([c_img, s_img], titles=['c', 's'])
+if __name__ == '__main__':
+    utils.show_images_with_objects([c_img, s_img], titles=['c', 's'])
