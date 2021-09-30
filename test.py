7eb8dd285f6695cca5761cf7c71e420082ffa290
Meng Zhang <zhang4@fzi.de>
2021-09-30 22:28:57 +0200

    modified ..

--- test.py
+++ test.py
@@ -10,8 +10,9 @@ c_img = utils.load_image(content_path)
 s_img = utils.load_image(style_path)
 
 if __name__ == '__main__':
+    plt.figure(figsize=(20, 12))
     plt.subplot(1, 2, 1)
     utils.imshow(c_img, 'content_img')
-    
+
     plt.subplot(1, 2, 2)
     utils.imshow(s_img, 'style_img')
